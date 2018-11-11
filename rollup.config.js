bd4e51c6dd03ba2483579260b15854a4253a3b1c
Rogin Farrer <rfarrer@wayfair.com>
2018-11-11 15:34:30 -0500

    clean up build

--- rollup.config.js
+++ rollup.config.js
@@ -1,18 +1,25 @@
 import {uglify} from 'rollup-plugin-uglify';
 import babel from 'rollup-plugin-babel';
+import commonjs from 'rollup-plugin-commonjs';
+
+// `npm run build` -> `production` is true
+// `npm run dev` -> `production` is false
+const production = !process.env.ROLLUP_WATCH;
 
 export default {
   input: 'src/Collapse.js',
-  external: ['react'],
+  external: ['react', 'raf'],
   plugins: [
     babel({
       exclude: 'node_modules/**'
     }),
-    uglify()
+    commonjs(),
+    production && uglify()
   ],
   output: {
     format: 'umd',
-    name: 'collapse',
+    name: 'ReactCollapsed',
+    sourcemap: true,
     globals: {
       react: 'React',
       raf: 'RAF'
