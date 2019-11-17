6e342382deacb945d7479879fbaf98cf3dd882f6
Rogin Farrer <rogin@roginfarrer.com>
2019-11-17 14:46:22 -0500

    Fixes SSR id mismatch (#33)
    Fixes #32

--- src/hooks.js
+++ src/hooks.js
@@ -1,4 +1,4 @@
-import {useState, useRef, useEffect, useMemo} from 'react';
+import {useState, useRef, useEffect} from 'react';
 
 export function useStateOrProps({isOpen, defaultOpen}) {
   const [open, setOpen] = useState(defaultOpen || false);
@@ -17,26 +17,17 @@ export function useEffectAfterMount(cb, dependencies) {
   }, dependencies);
 }
 
-function useEffectOnMount(cb) {
-  const justMounted = useRef(true);
-  useEffect(() => {
-    if (justMounted.current) {
-      return cb();
-    }
-    justMounted.current = false;
-  });
-}
-
+// Unique ID implementation borrowed from React UI :)
+// https://github.com/reach/reach-ui/blob/6e9dbcf716d5c9a3420e062e5bac1ac4671d01cb/packages/auto-id/src/index.js
 let idCounter = 0;
+const genId = () => ++idCounter;
 
 /**
  * This generates a unique ID for an instance of Collapse
  * @return {String} the unique ID
  */
 export function useUniqueId() {
-  let counter = useMemo(() => idCounter++, []);
-  useEffectOnMount(() => {
-    counter++;
-  });
-  return counter;
+  const [id, setId] = useState(null);
+  useEffect(() => setId(genId()), []);
+  return id;
 }
